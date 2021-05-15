9d50eb979231fb75e0dbe6c8ef90fa67dc063ef0
nadia <nadia@10.67.105.187>
2021-05-15 16:38:44 -0400

    First commit

---
+++ src/test/java/com/common/Base.java
@@ -0,0 +1,32 @@
+package com.common;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+public	WebDriver  driver;
+	
+	public void getDriver() {
+
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+
+	}
+}
